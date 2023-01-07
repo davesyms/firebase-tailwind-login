2464adeee37245fe3ea45ecf5c714a19206aaef8
Dave Syms <davewsyms@gmail.com>
2023-01-06 20:22:24 -0700

    Create validation functions for email+pass

---
+++ utils/(auth)/FieldValidations.ts
@@ -0,0 +1,23 @@
+import { SignInError } from "../../pages/api/(signin)/SignInTypes";
+export const validateEmail = (email: string): SignInError | undefined => {
+    const re: string =
+        "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+    return email.match(re) ? undefined : "invalid-email";
+};
+
+export const validatePassword = (password: string): SignInError | undefined => {
+    // Check if password is at least 8 characters long
+    if (password.length < 6) {
+        return "Password should be at least 6 characters";
+    }
+    // Check if password contains at least one uppercase letter
+    if (!password.match(/[A-Z]/)) {
+        return "Password should contain at least one uppercase letter";
+    }
+    // Check if password contains at least one number
+    if (!password.match(/\d/)) {
+        return "Password should contain at least one number";
+    }
+
+    return undefined;
+};
