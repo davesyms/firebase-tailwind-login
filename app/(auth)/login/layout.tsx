f3e67da15584e341ff43b1156d78e67f8f3d63ef
Dave Syms <davewsyms@gmail.com>
2023-01-09 18:04:33 -0700

    Setup layout to allow for context usage

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,3 +1,18 @@
 "use client";
 import React from "react";
-import { AuthCon}
+import { AuthProvider } from "../../../context/authContext";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html>
+            <head />
+            <AuthProvider>
+                <body>{children}</body>
+            </AuthProvider>
+        </html>
+    );
+}
